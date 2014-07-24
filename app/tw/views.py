dedda076ddf7036b41e9d380835426dfcbcb2646
Talha Oz <toz@gmu.edu>
2014-07-24 12:55:41 -0400

    save to mongodb added

--- app/tw/views.py
+++ app/tw/views.py
@@ -12,6 +12,11 @@ import json
 from functools import partial
 import sys
 import csv
+import io
+from collections import defaultdict
+import pymongo
+from bson.json_util import dumps
+from datetime import datetime
 
 twitter = oauth.remote_app(    
      'twitter',
@@ -185,7 +190,10 @@ def get_friends_followers_ids(twitter_api, screen_name=None, user_id=None,
                                 count=5000)
 
     friends_ids, followers_ids = [], []
-    
+    ff = defaultdict(list)
+    ff['id'] = screen_name if screen_name else user_id
+    ff['created_time'] = datetime.now()
+
     for twitter_api_func, limit, ids, label in [
                     [get_friends_ids, friends_limit, friends_ids, "friends"], 
                     [get_followers_ids, followers_limit, followers_ids, "followers"]
@@ -211,6 +219,12 @@ def get_friends_followers_ids(twitter_api, screen_name=None, user_id=None,
         
             # XXX: You may want to store data during each iteration to provide an 
             # an additional layer of protection from exceptional circumstances
+            ff[label] = ids
+            #save_json("ff",dict(ff))
+            save_to_mongo(ff,"ddcss","ff")
+            mongo_reloaded = load_from_mongo("ddcss","ff")
+            print mongo_reloaded
+            print dumps(mongo_reloaded, ensure_ascii=False)
         
             if len(ids) >= limit or response is None:
                 break
@@ -265,3 +279,59 @@ def get_followers_ids(twitter_api, screen_name=None, user_id=None, followers_lim
 
     # Do something useful with the IDs, like store them to disk...
     return followers_ids[:followers_limit]
+
+
+def save_json(filename, data):
+    print data
+    print json.dumps(data, ensure_ascii=False)
+    with io.open('tests/{0}.json'.format(filename), 
+                 'w', encoding='utf-8') as f:
+        f.write(unicode(json.dumps(data, ensure_ascii=False)))
+
+def save_to_mongo(data, mongo_db, mongo_db_coll, save = True, manipulate=True ,**mongo_conn_kw):
+    
+    # Connects to the MongoDB server running on 
+    # localhost:27017 by default
+    
+    client = pymongo.MongoClient(**mongo_conn_kw)
+    
+    # Get a reference to a particular database
+    
+    db = client[mongo_db]
+    
+    # Reference a particular collection in the database
+    
+    coll = db[mongo_db_coll]
+    
+    # Perform a bulk insert and  return the IDs
+    
+    return coll.save(data)
+
+def load_from_mongo(mongo_db, mongo_db_coll, return_cursor=False,
+                    criteria=None, projection=None, **mongo_conn_kw):
+    
+    # Optionally, use criteria and projection to limit the data that is 
+    # returned as documented in 
+    # http://docs.mongodb.org/manual/reference/method/db.collection.find/
+    
+    # Consider leveraging MongoDB's aggregations framework for more 
+    # sophisticated queries.
+    
+    client = pymongo.MongoClient(**mongo_conn_kw)
+    db = client[mongo_db]
+    coll = db[mongo_db_coll]
+    
+    if criteria is None:
+        criteria = {}
+    
+    if projection is None:
+        cursor = coll.find(criteria)
+    else:
+        cursor = coll.find(criteria, projection)
+
+    # Returning a cursor is recommended for large amounts of data
+    
+    if return_cursor:
+        return cursor
+    else:
+        return [ item for item in cursor ]
